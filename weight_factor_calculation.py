1fe791d63f95d69d1a2fe58a154a66224dc0003c
lukasgartmair <lukas_gartmair@gmx.de>
2016-12-27 12:16:11 +0100

    dashed lines

--- weight_factor_calculation.py
+++ weight_factor_calculation.py
@@ -7,10 +7,12 @@ Created on Tue Dec 27 11:06:51 2016
 """
 import numpy as np
 import matplotlib.pyplot as pl
-from mpl_toolkits.mplot3d import axes3d
 import numpy.polynomial.polynomial as poly
 
-number_of_points = 40
+number_of_points = 50
+
+lineStyles = {':': '_draw_dotted', '--': '_draw_dashed', '-.': '_draw_dash_dot', '-': '_draw_solid'}
+line_styles_list = list(lineStyles.keys())
 
 ## case parallel
 edge_length = 1
@@ -28,11 +30,12 @@ volume_bottom_cube = side_a_bottom_cube * side_b_bottom_cube * side_c_bottom_cub
 right_contribution_case2_3d = (volume_cube - volume_bottom_cube) / volume_cube
 right_contribution_case2_3d[right_contribution_case2_3d >= 1] = 1
 
-
 wrong_contribution_case2_3d = 1 - right_contribution_case2_3d
 
-pl.scatter(distance_to_surface, right_contribution_case2_3d, c='green', label='parallel')
+fig2 = pl.figure()
+ax2 = fig2.add_subplot(111)
 
+ax2.plot(distance_to_surface, right_contribution_case2_3d, linestyle=line_styles_list[1] , color=str(0/10) ,linewidth=2.0, label='parallel')
 
 # case rotated
 vertex_angle_bottom_triangle_rad = np.deg2rad(45)
@@ -45,14 +48,11 @@ volume_bottom_prism = 0.5 *  height_bottom_prism * base_length_bottom_pyramid * 
 right_contribution_case1_3d = (volume_cube - volume_bottom_prism)/volume_cube
 wrong_contribution_case1_3d = 1 - right_contribution_case1_3d
 
-pl.scatter(distance_to_surface, right_contribution_case1_3d, label='rotated')
+ax2.plot(distance_to_surface, right_contribution_case1_3d,  linestyle=line_styles_list[3] , color=str(1/10), linewidth=2.0, label='rotated')
 
 mean_contribution = (right_contribution_case1_3d + right_contribution_case2_3d) / 2
 
-pl.scatter(distance_to_surface, mean_contribution, c='red', label='mean')
-
-pl.xlabel('distance to surface')
-pl.ylabel('right_contribution')
+ax2.plot(distance_to_surface, mean_contribution,  linestyle=line_styles_list[2] , linewidth=2.0,color=str(2/10), label='mean')
 
 # polynomial fit
 # http://stackoverflow.com/questions/19165259/python-numpy-scipy-curve-fitting
@@ -64,7 +64,10 @@ f = np.poly1d(z)
 # calculate new x's and y's
 y_new = f(distance_to_surface)
 
-pl.plot(distance_to_surface, y_new, label='polyfit mean')
+ax2.plot(distance_to_surface, y_new,color=str(2/10) , linestyle=line_styles_list[0], linewidth=2.0, label='polyfit mean')
+pl.xlabel('distance to surface')
+pl.ylabel('right_contribution')
+pl.grid(True)
 pl.legend(loc='lower right')
 
 # test case parallel aligned voxel with distance = 0.25 and edge length = 1 -> right contribution = 0.75
